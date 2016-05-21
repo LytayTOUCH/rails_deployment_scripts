f55de603fe2a379e5572c5f71d776fd28a83a463
LytayTOUCH <lytaytouch@gmail.com>
2016-05-21 19:09:45 +0700

    Update imagemagick version and debian package

--- setup-imagemagick.sh
+++ setup-imagemagick.sh
@@ -8,10 +8,10 @@ sudo apt-get update -y
 sudo apt-get install build-essential checkinstall libx11-dev libxext-dev zlib1g-dev libpng12-dev libjpeg-dev libfreetype6-dev libxml2-dev libmagickwand-dev -y
 
 echo -e "---- Download ImageMagick Package ----"
-wget http://www.imagemagick.org/download/ImageMagick-6.9.3-7.tar.gz
+wget http://www.imagemagick.org/download/ImageMagick-6.9.4-3.tar.gz
 
 echo -e "---- Installing ImageMagick Package ----"
-tar -xzvf ImageMagick-6.9.3-7.tar.gz
-cd ImageMagick-6.9.3-7
+tar -xzvf ImageMagick-6.9.4-3.tar.gz
+cd ImageMagick-6.9.4-3
 ./configure
 sudo checkinstall
